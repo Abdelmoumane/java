ee5e8acd8c45e6f481eb53ad62c378b14f24e352
abdel <abdel>
2025-02-23 22:40:57 +0100

    new

---
+++ JavaFx.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javafx;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import static java.time.Instant.now;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.InputMismatchException;
+
+
+
+
+
+import java.util.Scanner;
+
+public class JavaFx {
+    public static void main(String[] args) {
+        
+        
+        Scanner scan = new Scanner (System.in);
+        
+        System.out.println("introduce cadena");
+        
+        
+        String numbre = scan.nextLine();
+        
+        
+        int a =0;
+        int e=0;
+        
+        
+        
+     
+        for ( char ch : numbre.toCharArray())
+      
+            
+            if ("aei".indexOf(ch) != -1){
+                
+                
+        
+            }
+        
+        
+        System.out.println("e: "+e+" a: "+a);
+    }
+}
+   
+    
+
